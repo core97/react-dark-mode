250be12756b181e237aa1c847d624bd131c2f199
Juan Cortés <jcortes@llorenteycuenca.com>
2020-03-06 14:35:27 +0100

    created effect dark mode

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).  
+Thanks for [tutorial](https://www.youtube.com/watch?v=FjDo1mbkNtw)
 
 ## Available Scripts
 
--- src/App.js
+++ src/App.js
@@ -1,12 +1,55 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.scss';
 
 function App() {
+  const [darkMode, setDarkMode] = useState(getInitialMode());
+  useEffect(() => {
+    localStorage.setItem('dark', JSON.stringify(darkMode));
+  }, [darkMode]);
+
+  function getInitialMode() {
+    const isReturningUser = "dark" in localStorage;
+    const savedMode = JSON.parse(localStorage.getItem('dark'));
+    const userPrefersDark = getPrefColorsScheme();
+    // if mode was saved -> dark / light
+    if (isReturningUser) {
+      return savedMode;
+    } else if (userPrefersDark) {
+      // if prefered color scheme is dark -> dark
+      return true;
+    } else {
+      // otherwise -> light
+      return false;
+    }
+  }
+
+  function getPrefColorsScheme() {
+    if (!window.matchMedia) return;
+
+    // return true or false
+    return window.matchMedia("(prefers-color-scheme: dark)").matches;
+  }
+
   return (
-    <div className="light-mode">
-      <nav>Toggle Goes Here</nav>
+    <div className={darkMode ? "dark-mode" : "light-mode"}>
+      <nav>
+        <div className="toggle-container">
+          <span style={{color: darkMode ? "grey" : "yellow"}}>☀</span>
+          <span className="toggle">
+            <input
+              checked={darkMode}
+              onChange={() => setDarkMode(prevMode => !prevMode)}
+              type="checkbox"
+              className="checkbox"
+              id="checkbox"
+            />
+            <label htmlFor="checkbox"></label>
+          </span>
+          <span style={{color: darkMode ? "slateblue" : "grey"}}>☾</span>
+        </div>
+      </nav>
       <main>
-        <h1>Light Mode</h1>
+        <h1>{darkMode ? "Dark Mode" : "Light Mode"}</h1>
         <h2>Toggle the switch to see some magic happen!</h2>
       </main>
     </div>
