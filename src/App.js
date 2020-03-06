f14c037b0a0be202c4eabac831f9ee75db12b8b2
core97 <jcortesg1997@gmail.com>
2020-03-06 19:51:03 +0100

    Added switch with material-ui

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React, { useState, useEffect } from 'react';
+import Switch from '@material-ui/core/Switch';
 import './App.scss';
 
 function App() {
   const [darkMode, setDarkMode] = useState(getInitialMode());
+
   useEffect(() => {
     localStorage.setItem('dark', JSON.stringify(darkMode));
   }, [darkMode]);
 
   function getInitialMode() {
-    const isReturningUser = "dark" in localStorage;
+    const isReturningUser = 'dark' in localStorage;
     const savedMode = JSON.parse(localStorage.getItem('dark'));
     const userPrefersDark = getPrefColorsScheme();
     // if mode was saved -> dark / light
@@ -25,32 +27,28 @@ function App() {
 
   function getPrefColorsScheme() {
     if (!window.matchMedia) return;
-
     // return true or false
-    return window.matchMedia("(prefers-color-scheme: dark)").matches;
+    return window.matchMedia('(prefers-color-scheme: dark)').matches;
   }
 
   return (
-    <div className={darkMode ? "dark-mode" : "light-mode"}>
+    <div className={darkMode ? 'dark-mode' : 'light-mode'}>
       <nav>
         <div className="toggle-container">
-          <span style={{color: darkMode ? "grey" : "yellow"}}>☀</span>
+          <span style={{ color: darkMode ? 'grey' : 'yellow' }}>☀</span>
           <span className="toggle">
-            <input
+            <Switch
               checked={darkMode}
               onChange={() => setDarkMode(prevMode => !prevMode)}
-              type="checkbox"
-              className="checkbox"
-              id="checkbox"
+              color="primary"
+              inputProps={{ 'aria-label': 'primary checkbox' }}
             />
-            <label htmlFor="checkbox"></label>
           </span>
-          <span style={{color: darkMode ? "slateblue" : "grey"}}>☾</span>
+          <span style={{ color: darkMode ? 'slateblue' : 'grey' }}>☾</span>
         </div>
       </nav>
       <main>
-        <h1>{darkMode ? "Dark Mode" : "Light Mode"}</h1>
-        <h2>Toggle the switch to see some magic happen!</h2>
+        <h1>{darkMode ? 'Dark Mode' : 'Light Mode'}</h1>
       </main>
     </div>
   );
